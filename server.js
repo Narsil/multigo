0f4e629995a30bca0f440c87616e23eb2ee3a87b
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-31 00:44:51 +0200

    Small changes to the initial code in server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var server = http.createServer(function(request, response){
 	response.writeHead(200,{
 		'Content-type':  'text/plain'
 	});
-	response.end("Hello World!");
+	response.write("Hello World!");
+	response.end();
 });
 
 server.listen(8080); 
