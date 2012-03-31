ce178c94b852ca29208aceba0412d2075415b9eb
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-31 12:24:06 +0200

    Changing the way config files are handled (creating symlinks to corresponding config files depending on environement)

---
+++ config-dev.js
@@ -0,0 +1,7 @@
+//DEV CONFIG FILE
+
+var URL = "127.0.0.1";
+var PORT = 8080;
+
+exports.URL = URL;
+exports.PORT = PORT;
---
+++ config-prod.js
@@ -0,0 +1,7 @@
+//PROD CONFIG FILE
+
+var URL = "go.kwyk.fr";
+var PORT = 80;
+
+exports.URL = URL;
+exports.PORT = PORT;
