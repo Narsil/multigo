e4931f4001df6a173914f05da2e3cda744890860
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-31 10:43:44 +0200

    Switching to modules architecture

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var http = require("http");
 
-var server = http.createServer(function(request, response){
-	response.writeHead(200,{
-		'Content-type':  'text/plain'
-	});
-	response.write("Hello World!");
-	response.end();
-});
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
 
-server.listen(8080); 
+  http.createServer(onRequest).listen(8080);
+  console.log("Server has started.");
+}
 
 console.log("Listening on http://127.0.0.1:8080");
+
+exports.start = start;
+
