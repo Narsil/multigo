a5dbfd8b093b81a198b723a900c25552bc2ef6b1
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-03-31 13:15:24 +0200

    Forgot to add files to commit ^^'

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,13 @@
+var fs = require("fs");
+
+var indexHTML = fs.readFileSync("views/index.html");
+
 function index(response){
 	console.log("Request handler 'index' was called.");
+	response.writeHead(200, {
+		'Content-type': 'text/html; charset=utf-8'
+	});
+	response.end(indexHTML);
 }
 
 exports.index = index;
--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
     handle[pathname](response);
