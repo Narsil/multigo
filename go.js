51d3c2c642854bd6c005b90a10701c944955c421
Nicolas Patry <nicolas@kwyk.fr>
2012-04-02 10:49:30 +0200

    Corrected a stone taking in go.js.

--- go.js
+++ go.js
@@ -8,6 +8,7 @@ var contains = function(list, coord){
 };
 
 var take_stone_if = function(state, x, y, checked){
+    var should_check = [];
     for (var i=0;i<4;i++){
         switch(i){
             case 0:
@@ -30,29 +31,40 @@ var take_stone_if = function(state, x, y, checked){
         if (state[ix] === undefined || state[ix][iy] === undefined){
             continue;
         }else if(state[ix][iy] <= 0){
-            return state;
+            return {'state': state, 'taken':false};
         }else if (contains(checked, [ix, iy]) || state[ix][iy] != state[x][y]){
             continue;
         }else if (state[ix][iy] == state[x][y]){
-            checked.push([x, y]);
-            state = take_stone_if(state, ix, iy, checked);
+            should_check.push([ix, iy]);
         }
     }
     checked.push([x, y]);
-    for (i = 0; i < checked.length; i++){
-        var coord = checked[i];
-        var lx = coord[0];
-        var ly = coord[1];
-        state[lx][ly] = 0;
+    var taken = true;
+    var coord;
+    for (i = 0; i < should_check.length; i++){
+        coord = should_check[i];
+        ix = coord[0];
+        iy = coord[1];
+        var take_status = take_stone_if(state, ix, iy, checked);
+        taken =  take_status.taken;
     }
-    return state;
+    if (taken){
+        for (i = 0; i < checked.length; i++){
+            coord = checked[i];
+            var lx = coord[0];
+            var ly = coord[1];
+            state[lx][ly] = 0;
+        }
+    }
+    return {'state': state, 'taken':true};
 };
 
 var check_stone = function(state, x, y, ix, iy){
     if (state[ix] === undefined || state[ix][iy] === undefined || state[ix][iy] === 0 || state[ix][iy] == state[x][y]){
         return state;
     }else{
-        state = take_stone_if(state, ix, iy, []);
+        taken_status = take_stone_if(state, ix, iy, []);
+        state = taken_status.state;
     }
     return state;
 };
