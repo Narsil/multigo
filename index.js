01582253f5fb55007de96ff54494dd321a62fc7b
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2012-04-14 10:45:43 +0200

    Reorganising code, removing useless files

--- index.js
+++
@@ -1,9 +0,0 @@
-var server = require("./server.js");
-var router = require("./router.js");
-var requestHandlers = require("./requestHandlers");
-
-var handle = {};
-
-handle["/"] = requestHandlers.index;
-
-server.start(router.route, handle);
